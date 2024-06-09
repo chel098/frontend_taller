de542dbdd91b773318e5ce386754ff69fa478d6c
chel098 <xeli6842@gmail.com>
2024-06-09 00:59:11 -0400

    Creacion

---
+++ frontend_taller/src/app/app.routes.ts
@@ -0,0 +1,3 @@
+import { Routes } from '@angular/router';
+import { NewClienteComponent } from './new-cliente/new-cliente.component';
+export const routes: Routes = [{ path: 'new-cliente', component: NewClienteComponent}];
---
+++ frontend_taller/src/app/new-cliente/new-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-cliente.component.html',
+  styleUrl: './new-cliente.component.css'
+})
+export class NewClienteComponent {
+
+}
