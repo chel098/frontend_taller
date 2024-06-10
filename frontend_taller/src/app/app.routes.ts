440dd959bcd5a70541619218f0c4bb2a84c67e19
chel098 <xeli6842@gmail.com>
2024-06-10 00:48:09 -0400

    adicion

--- frontend_taller/src/app/app.routes.ts
+++ frontend_taller/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 import { NewClienteComponent } from './new-cliente/new-cliente.component';
-export const routes: Routes = [{ path: 'new-cliente', component: NewClienteComponent}];
+import { EditClienteComponent } from './edit-cliente/edit-cliente.component';
+export const routes: Routes = [{ path: 'new-cliente', component: NewClienteComponent},
+{ path: 'edit-cliente', component: EditClienteComponent}
+];
---
+++ frontend_taller/src/app/edit-cliente/edit-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-cliente.component.html',
+  styleUrl: './edit-cliente.component.css'
+})
+export class EditClienteComponent {
+
+}
